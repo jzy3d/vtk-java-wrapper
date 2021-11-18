378c7a110540224a4f20175eab4f4081f1a4c89d
martin <martin@jzy3d.org>
2021-11-18 11:52:49 +0100

    Let tests check VTK version

--- src/test/java/org/jzy3d/io/vtk/drawable/TestVTKDrawableVBOBuilder.java
+++ src/test/java/org/jzy3d/io/vtk/drawable/TestVTKDrawableVBOBuilder.java
@@ -16,6 +16,7 @@ import vtk.vtkAlgorithm;
 import vtk.vtkGeometryFilter;
 import vtk.vtkPolyData;
 import vtk.vtkUnstructuredGrid;
+import vtk.vtkVersion;
 
 /**
  * These tests will have the classpath to VTK native automatically added by Maven Surefire Plugin
@@ -32,14 +33,18 @@ public class TestVTKDrawableVBOBuilder {
   int HEXAHEDRON_FACES = 6;
   int QUAD_POINTS = 4;
 
+  /* VTK Geometry filter behaves differently in 9.1.0 and 9.0.1 */
+  static boolean is_VTK_9_1;
+
   @BeforeClass
   public static void load() {
     VTKUtils.loadVtkNativeLibraries();
+    is_VTK_9_1 = "9.1.0".equals(new vtkVersion().GetVTKVersion());
   }
-  
+
   ///////////////////////////////////////////////////////////////////
   //
-  //                            SHARED
+  // SHARED
   //
   ///////////////////////////////////////////////////////////////////
 
@@ -55,7 +60,7 @@ public class TestVTKDrawableVBOBuilder {
 
     // --------------------------------------------
     // Given a file with hexahedrons inside
-    
+
     vtkUnstructuredGrid grid = VTKReader
         .getReaderOutput("./src/test/resources/Enthalpy_Cylinder/Enthalpy_HS_cylinder_080.pvtu");
 
@@ -65,7 +70,7 @@ public class TestVTKDrawableVBOBuilder {
 
     // --------------------------------------------
     // When load, make and add to chart
-    
+
     VTKDrawableVBOBuilder builder = new VTKDrawableVBOBuilder(grid, GeometryMode.MULTI_GEOMETRY,
         VerticeMode.SHARED, VTKGeometry.VTK_HEXAHEDRON);
 
@@ -73,34 +78,34 @@ public class TestVTKDrawableVBOBuilder {
 
     chart.add(vbo);
 
-    
+
     // --------------------------------------------
     // Then buffers have the appropriate size
-    
+
     Assert.assertEquals(EXPECT_POINTS * 3, vbo.getVertices().capacity());
     Assert.assertEquals(EXPECT_POINTS * 3, vbo.getNormals().capacity());
     Assert.assertEquals(EXPECT_POINTS * 3, vbo.getColors().capacity());
 
     // input hexahedron are loaded as 6 quads in the VBO
-    Assert.assertEquals(EXPECT_CELLS * HEXAHEDRON_FACES, vbo.getElementsIndices().capacity()); 
+    Assert.assertEquals(EXPECT_CELLS * HEXAHEDRON_FACES, vbo.getElementsIndices().capacity());
     Assert.assertEquals(EXPECT_CELLS * HEXAHEDRON_FACES, vbo.getElementsCount().capacity());
-    Assert.assertEquals(QUAD_POINTS, vbo.getElementsCount().get(0)); 
+    Assert.assertEquals(QUAD_POINTS, vbo.getElementsCount().get(0));
 
     // these are not used
     Assert.assertNull(vbo.getElements());
     Assert.assertNull(vbo.getElementsLength());
     Assert.assertNull(vbo.getElementsStarts());
 
-    
+
     // --------------------------------------------
     // When load another property, Then size is correct
 
     float[] colors = builder.getPropertyColor("marker1");
 
     Assert.assertEquals(EXPECT_POINTS * 3, colors.length);
-    
+
   }
-  
+
   @Test
   public void givenSharedVerticeQuads() {
     ChartFactory f = new AWTChartFactory();
@@ -109,18 +114,18 @@ public class TestVTKDrawableVBOBuilder {
 
     int EXPECT_CELLS = 7160;
     int EXPECT_POINTS = 9114;
-    int EXPECT_POINTS_POST_FILTER = 6197;
-    
+    int EXPECT_POINTS_POST_FILTER = is_VTK_9_1 ? 6197 : EXPECT_POINTS;
+
 
     // --------------------------------------------
     // Given a file with hexahedrons inside ...
-    
-    vtkAlgorithm reader = VTKReader
-        .getReader("./src/test/resources/Enthalpy_Cylinder/Enthalpy_HS_cylinder_080.pvtu");
 
-    
+    vtkAlgorithm reader =
+        VTKReader.getReader("./src/test/resources/Enthalpy_Cylinder/Enthalpy_HS_cylinder_080.pvtu");
+
+
     // ... converted to quads
-    
+
     vtkGeometryFilter quadConversion = new vtkGeometryFilter();
     quadConversion.SetInputConnection(reader.GetOutputPort());
     quadConversion.Update();
@@ -134,13 +139,13 @@ public class TestVTKDrawableVBOBuilder {
 
     // --------------------------------------------
     // When load, make and add to chart
-    
+
     VTKDrawableVBOBuilder builder = new VTKDrawableVBOBuilder(quads, GeometryMode.MULTI_GEOMETRY,
         VerticeMode.SHARED, VTKGeometry.VTK_QUAD);
 
     // --------------------------------------------
     // Then buffers have the appropriate size
-    
+
     DrawableVBO2 vbo = builder.makePolygons("enthalpy");
 
     chart.add(vbo);
@@ -149,16 +154,16 @@ public class TestVTKDrawableVBOBuilder {
     Assert.assertEquals(EXPECT_POINTS_POST_FILTER * 3, vbo.getColors().capacity());
 
     // input quads are loaded as ... quads in the VBO
-    Assert.assertEquals(EXPECT_CELLS , vbo.getElementsIndices().capacity());
+    Assert.assertEquals(EXPECT_CELLS, vbo.getElementsIndices().capacity());
     Assert.assertEquals(EXPECT_CELLS, vbo.getElementsCount().capacity());
-    Assert.assertEquals(QUAD_POINTS , vbo.getElementsCount().get(0));
+    Assert.assertEquals(QUAD_POINTS, vbo.getElementsCount().get(0));
 
     // these are not used
     Assert.assertNull(vbo.getElements());
     Assert.assertNull(vbo.getElementsLength());
     Assert.assertNull(vbo.getElementsStarts());
-    
-    
+
+
     // --------------------------------------------
     // When load another property, Then size is correct
 
@@ -168,14 +173,14 @@ public class TestVTKDrawableVBOBuilder {
 
 
   }
-  
-  
+
+
   ///////////////////////////////////////////////////////////////////
   //
-  //                          REPEATED
+  // REPEATED
   //
   ///////////////////////////////////////////////////////////////////
-  
+
   @Test
   public void givenRepeatedVerticeHexahedron() {
     ChartFactory f = new AWTChartFactory();
@@ -187,7 +192,7 @@ public class TestVTKDrawableVBOBuilder {
 
     // --------------------------------------------
     // Given a file with hexahedrons inside
-    
+
     vtkUnstructuredGrid grid = VTKReader
         .getReaderOutput("./src/test/resources/Enthalpy_Cylinder/Enthalpy_HS_cylinder_080.pvtu");
 
@@ -197,7 +202,7 @@ public class TestVTKDrawableVBOBuilder {
 
     // --------------------------------------------
     // When load, make and add to chart
-    
+
     VTKDrawableVBOBuilder builder = new VTKDrawableVBOBuilder(grid, GeometryMode.MULTI_GEOMETRY,
         VerticeMode.REPEATED, VTKGeometry.VTK_HEXAHEDRON);
 
@@ -207,26 +212,26 @@ public class TestVTKDrawableVBOBuilder {
 
     chart.add(vbo);
 
-    
+
     // --------------------------------------------
     // Then buffers have the appropriate size
-    
+
     Assert.assertEquals(EXPECT_POINTS * 3, vbo.getVertices().capacity());
     Assert.assertEquals(EXPECT_POINTS * 3, vbo.getNormals().capacity());
     Assert.assertEquals(EXPECT_POINTS * 3, vbo.getColors().capacity());
 
-    
+
     // input quads are loaded as ... quads in the VBO
-    Assert.assertEquals(EXPECT_CELLS*HEXAHEDRON_FACES, vbo.getElementsStarts().capacity()); // ??
-    Assert.assertEquals(EXPECT_CELLS*HEXAHEDRON_FACES, vbo.getElementsLength().capacity()); // ??
-    Assert.assertEquals(QUAD_POINTS , vbo.getElementsLength().get(0));
+    Assert.assertEquals(EXPECT_CELLS * HEXAHEDRON_FACES, vbo.getElementsStarts().capacity()); // ??
+    Assert.assertEquals(EXPECT_CELLS * HEXAHEDRON_FACES, vbo.getElementsLength().capacity()); // ??
+    Assert.assertEquals(QUAD_POINTS, vbo.getElementsLength().get(0));
 
     // these are not used
     Assert.assertNull(vbo.getElements());
-    Assert.assertNull(vbo.getElementsIndices()); 
+    Assert.assertNull(vbo.getElementsIndices());
     Assert.assertNull(vbo.getElementsCount());
-    
-    
+
+
     // --------------------------------------------
     // When load another property, Then size is correct
 
@@ -234,10 +239,10 @@ public class TestVTKDrawableVBOBuilder {
 
     Assert.assertEquals(EXPECT_POINTS * 3, colors.length);
 
-    
+
   }
-  
-  
+
+
   @Test
   public void givenRepeatedVerticeQuads() {
     ChartFactory f = new AWTChartFactory();
@@ -246,39 +251,39 @@ public class TestVTKDrawableVBOBuilder {
 
     int EXPECT_CELLS = 7160;
     int EXPECT_POINTS = 9114;
-    int EXPECT_POINTS_POST_FILTER = 6197;
+    int EXPECT_POINTS_POST_FILTER = is_VTK_9_1 ? 6197 : EXPECT_POINTS;
 
 
     // --------------------------------------------
     // Given a file with hexahedrons inside ...
-    
-    vtkAlgorithm reader = VTKReader
-        .getReader("./src/test/resources/Enthalpy_Cylinder/Enthalpy_HS_cylinder_080.pvtu");
 
-    
+    vtkAlgorithm reader =
+        VTKReader.getReader("./src/test/resources/Enthalpy_Cylinder/Enthalpy_HS_cylinder_080.pvtu");
+
+
     // ... converted to quads
-    
+
     vtkGeometryFilter quadConversion = new vtkGeometryFilter();
     quadConversion.SetInputConnection(reader.GetOutputPort());
     quadConversion.Update();
 
     vtkPolyData quads = quadConversion.GetOutput();
-    
+
 
     Assert.assertEquals(EXPECT_CELLS, quads.GetNumberOfCells());
     Assert.assertEquals(EXPECT_POINTS_POST_FILTER, quads.GetNumberOfPoints());
 
     // --------------------------------------------
     // When load, make and add to chart
-    
+
     VTKDrawableVBOBuilder builder = new VTKDrawableVBOBuilder(quads, GeometryMode.MULTI_GEOMETRY,
         VerticeMode.REPEATED, VTKGeometry.VTK_QUAD);
 
     // --------------------------------------------
     // Then buffers have the appropriate size
-    
+
     DrawableVBO2 vbo = builder.makePolygons("enthalpy");
-    
+
     Assert.assertEquals(3, builder.getColorChannels());
 
     chart.add(vbo);
@@ -288,16 +293,16 @@ public class TestVTKDrawableVBOBuilder {
     Assert.assertEquals(EXPECT_POINTS_POST_FILTER * 3, vbo.getColors().capacity());
 
     // input quads are loaded as ... quads in the VBO
-    Assert.assertEquals(EXPECT_CELLS*QUAD_POINTS, vbo.getElementsStarts().capacity()); // ??
-    Assert.assertEquals(EXPECT_CELLS*QUAD_POINTS , vbo.getElementsLength().capacity()); // ??
-    Assert.assertEquals(QUAD_POINTS , vbo.getElementsLength().get(0));
+    Assert.assertEquals(EXPECT_CELLS * QUAD_POINTS, vbo.getElementsStarts().capacity()); // ??
+    Assert.assertEquals(EXPECT_CELLS * QUAD_POINTS, vbo.getElementsLength().capacity()); // ??
+    Assert.assertEquals(QUAD_POINTS, vbo.getElementsLength().get(0));
 
     // these are not used
     Assert.assertNull(vbo.getElements());
-    Assert.assertNull(vbo.getElementsIndices()); 
+    Assert.assertNull(vbo.getElementsIndices());
     Assert.assertNull(vbo.getElementsCount());
 
-    
+
     // --------------------------------------------
     // When load another property, Then size is correct
 
@@ -306,10 +311,10 @@ public class TestVTKDrawableVBOBuilder {
     Assert.assertEquals(EXPECT_POINTS_POST_FILTER * 3, colors.length);
 
   }
-  
+
   ///////////////////////////////////////////////////////////////////
   //
-  //                          ALPHA
+  // ALPHA
   //
   ///////////////////////////////////////////////////////////////////
 
@@ -320,31 +325,31 @@ public class TestVTKDrawableVBOBuilder {
     Chart chart = f.newChart();
 
     int EXPECT_POINTS = 9114;
-    int EXPECT_POINTS_POST_FILTER = 6197;
+    int EXPECT_POINTS_POST_FILTER = is_VTK_9_1?6197:EXPECT_POINTS;
 
-    
+
     float ALPHA = 0.5f;
     int ALPHA_INDEX = 3;
 
     // --------------------------------------------
     // Given a file with hexahedrons inside
-    
-    vtkAlgorithm reader = VTKReader
-        .getReader("./src/test/resources/Enthalpy_Cylinder/Enthalpy_HS_cylinder_080.pvtu");
 
-    
+    vtkAlgorithm reader =
+        VTKReader.getReader("./src/test/resources/Enthalpy_Cylinder/Enthalpy_HS_cylinder_080.pvtu");
+
+
     // ... converted to quads
-    
+
     vtkGeometryFilter quadConversion = new vtkGeometryFilter();
     quadConversion.SetInputConnection(reader.GetOutputPort());
     quadConversion.Update();
 
     vtkPolyData quads = quadConversion.GetOutput();
 
-    
+
     // --------------------------------------------
     // When load, make and add to chart
-    
+
     VTKDrawableVBOBuilder builder = new VTKDrawableVBOBuilder(quads);
 
     Assert.assertEquals(3, builder.getColorChannels()); // does not contains alpha yet
@@ -356,31 +361,35 @@ public class TestVTKDrawableVBOBuilder {
     DrawableVBO2 vbo = builder.makePolygons("enthalpy");
 
     chart.add(vbo);
-    
+
     // --------------------------------------------
     // Then buffers have the appropriate size
-    
-    Assert.assertEquals(EXPECT_POINTS_POST_FILTER * 4, vbo.getColors().capacity()); // contains alpha
 
-    Assert.assertEquals(ALPHA, vbo.getColors().get(0+ALPHA_INDEX)); // alpha properly set for a few point
-    Assert.assertEquals(ALPHA, vbo.getColors().get(4+ALPHA_INDEX)); // alpha properly set for a few point
-    Assert.assertEquals(ALPHA, vbo.getColors().get(8+ALPHA_INDEX)); // alpha properly set for a few point
+    Assert.assertEquals(EXPECT_POINTS_POST_FILTER * 4, vbo.getColors().capacity()); // contains
+                                                                                    // alpha
+
+    Assert.assertEquals(ALPHA, vbo.getColors().get(0 + ALPHA_INDEX)); // alpha properly set for a
+                                                                      // few point
+    Assert.assertEquals(ALPHA, vbo.getColors().get(4 + ALPHA_INDEX)); // alpha properly set for a
+                                                                      // few point
+    Assert.assertEquals(ALPHA, vbo.getColors().get(8 + ALPHA_INDEX)); // alpha properly set for a
+                                                                      // few point
 
     Assert.assertEquals(4, vbo.getColorChannels()); // contains alpha AFTER being loaded
-    
+
     // --------------------------------------------
     // When load another property, Then size is correct
 
     float[] colors = builder.getPropertyColor("marker1");
 
     Assert.assertEquals(EXPECT_POINTS_POST_FILTER * 4, colors.length); // contains alpha
-    
-    Assert.assertEquals(ALPHA, colors[0+ALPHA_INDEX]); // alpha properly set for a few point
-    Assert.assertEquals(ALPHA, colors[4+ALPHA_INDEX]); // alpha properly set for a few point
-    Assert.assertEquals(ALPHA, colors[8+ALPHA_INDEX]); // alpha properly set for a few point
-    
+
+    Assert.assertEquals(ALPHA, colors[0 + ALPHA_INDEX]); // alpha properly set for a few point
+    Assert.assertEquals(ALPHA, colors[4 + ALPHA_INDEX]); // alpha properly set for a few point
+    Assert.assertEquals(ALPHA, colors[8 + ALPHA_INDEX]); // alpha properly set for a few point
+
   }
-  
+
   @Test
   public void givenNonOpaqueSetting_Hexahedrons() {
     ChartFactory f = new AWTChartFactory();
@@ -388,19 +397,19 @@ public class TestVTKDrawableVBOBuilder {
     Chart chart = f.newChart();
 
     int EXPECT_POINTS = 9114;
-    
+
     float ALPHA = 0.5f;
     int ALPHA_INDEX = 3;
 
     // --------------------------------------------
     // Given a file with hexahedrons inside
-    
+
     vtkUnstructuredGrid grid = VTKReader
         .getReaderOutput("./src/test/resources/Enthalpy_Cylinder/Enthalpy_HS_cylinder_080.pvtu");
 
     // --------------------------------------------
     // When load, make and add to chart
-    
+
     VTKDrawableVBOBuilder builder = new VTKDrawableVBOBuilder(grid);
 
     Assert.assertEquals(3, builder.getColorChannels()); // does not contains alpha yet
@@ -412,29 +421,32 @@ public class TestVTKDrawableVBOBuilder {
     DrawableVBO2 vbo = builder.makePolygons("enthalpy");
 
     chart.add(vbo);
-    
+
     // --------------------------------------------
     // Then buffers have the appropriate size
-    
+
     Assert.assertEquals(EXPECT_POINTS * 4, vbo.getColors().capacity()); // contains alpha
 
-    Assert.assertEquals(ALPHA, vbo.getColors().get(0+ALPHA_INDEX)); // alpha properly set for a few point
-    Assert.assertEquals(ALPHA, vbo.getColors().get(4+ALPHA_INDEX)); // alpha properly set for a few point
-    Assert.assertEquals(ALPHA, vbo.getColors().get(8+ALPHA_INDEX)); // alpha properly set for a few point
+    Assert.assertEquals(ALPHA, vbo.getColors().get(0 + ALPHA_INDEX)); // alpha properly set for a
+                                                                      // few point
+    Assert.assertEquals(ALPHA, vbo.getColors().get(4 + ALPHA_INDEX)); // alpha properly set for a
+                                                                      // few point
+    Assert.assertEquals(ALPHA, vbo.getColors().get(8 + ALPHA_INDEX)); // alpha properly set for a
+                                                                      // few point
 
     Assert.assertEquals(4, vbo.getColorChannels()); // contains alpha AFTER being loaded
-    
+
     // --------------------------------------------
     // When load another property, Then size is correct
 
     float[] colors = builder.getPropertyColor("marker1");
 
     Assert.assertEquals(EXPECT_POINTS * 4, colors.length); // contains alpha
-    
-    Assert.assertEquals(ALPHA, colors[0+ALPHA_INDEX]); // alpha properly set for a few point
-    Assert.assertEquals(ALPHA, colors[4+ALPHA_INDEX]); // alpha properly set for a few point
-    Assert.assertEquals(ALPHA, colors[8+ALPHA_INDEX]); // alpha properly set for a few point
 
-    
+    Assert.assertEquals(ALPHA, colors[0 + ALPHA_INDEX]); // alpha properly set for a few point
+    Assert.assertEquals(ALPHA, colors[4 + ALPHA_INDEX]); // alpha properly set for a few point
+    Assert.assertEquals(ALPHA, colors[8 + ALPHA_INDEX]); // alpha properly set for a few point
+
+
   }
 }
